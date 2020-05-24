075ef4d7c2b15dc463fc5b8e12455ae1eadc2a2f
ArCeusJ2000 <3081666963@qq.com>
2020-05-24 20:41:41 +0800

    feat: personal information page

---
+++ src/views/user/personal.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="info-wrapper">
+        <a-tabs>
+            <a-tab-pane tab="个人信息" key="1">
+                <a-form-item :form="form" style="margin-top: 30px">
+
+                    <a-form-item label="用户名" :label-col="{ span: 3 }" :wrapper-col="{ span: 8, offset: 1  }">
+                        <a-input
+                                placeholder="请填写用户名"
+                                v-decorator="['userName', { rules: [{ required: true, message: '请输入用户名' }] }]"
+                                v-if="modify"
+                        />
+                        <span v-else>{{ userInfo.userName }}</span>
+
+                    <a-form-item label="手机号" :label-col="{ span: 3 }" :wrapper-col="{ span: 8, offset: 1 }">
+                        <a-input
+                                placeholder="请填写手机号"
+                                v-decorator="['phoneNumber', { rules: [{ required: true, message: '请输入手机号' }] }]"
+                                v-if="modify"
+                        />
+                        <span v-else>{{ userInfo.phoneNumber}}</span>
+                    </a-form-item>
+                    <a-form-item label="密码" :label-col="{ span: 3 }" :wrapper-col="{ span: 8, offset: 1 }" v-if="modify">
+                        <a-input
+                                placeholder="请输入新密码"
+                                v-decorator="['password', { rules: [{ required: true, message: '请输入新密码' }] }]"
+                        />
+                    </a-form-item>
+                    <a-form-item :wrapper-col="{ span: 12, offset: 5 }" v-if="modify">
+                        <a-button type="primary" @click="saveModify">
+                            保存
+                        </a-button>
+                        <a-button type="default" style="margin-left: 30px" @click="cancelModify">
+                            取消
+                        </a-button>
+                    </a-form-item>
+                    <a-form-item :wrapper-col="{ span: 8, offset: 4 }" v-else>
+                        <a-button type="primary" @click="modifyInfo">
+                            修改信息
+                        </a-button>
+                    </a-form-item>
+                    </a-form-item>
+                </a-form-item>
+            </a-tab-pane>
+        </a-tabs>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "personal"
+    }
+</script>
+
+<style scoped>
+
+</style>
