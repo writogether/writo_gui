13dbb833ce7bb95cd422ecba610d4ae3988bbdf4
ArCeusJ2000 <3081666963@qq.com>
2020-05-24 09:37:42 +0800

    login

---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from '../views/login.vue'
+
+Vue.use(VueRouter)
+const routes = [
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/',
+    redirect: '/NJUSE'
+  },
+  {
+    path: '/NJUSE',
+    name: 'layout',
+    redirect: '/page',
+    component: ()=> import('@/views/layout'),
+
+  },
+]
+const createRouter = () => new VueRouter({
+  // mode: 'history', // require service support
+  scrollBehavior: () => ({ y: 0 }),
+  routes
+})
+const router = createRouter()
+
+export function resetRouter() {
+  const newRouter = createRouter()
+  router.matcher = newRouter.matcher // reset router
+}
+
+export default router
