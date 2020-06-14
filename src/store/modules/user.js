56458485b753e8af970b980d32870a6028e574ce
Turmiht <g13395930368@gmail.com>
2020-06-14 16:52:04 +0800

    fix: login

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,113 +1,90 @@
-import Vue from 'vue'
-import router from '../../router'
-import { getToken, setToken, removeToken } from '../../utils/auth'
-import { resetRouter } from '../../router'
-import { message } from 'ant-design-vue'
-import {
-    loginAPI,
-    registerAPI,
-    getUserInfoAPI,
-    updateUserInfoAPI,
-} from '../../api/user'
+import router, {resetRouter} from '../../router';
+import {removeToken} from '../../utils/auth';
+import {message} from 'ant-design-vue';
+import {getUserInfoAPI, loginAPI, registerAPI, updateUserInfoAPI} from '../../api/user';
 
 const getDefaultState = () => {
     return {
         userId: '',
-        userInfo: {
-
-        },
-        userOrderList: [
-
-        ]
-    }
-}
+        userInfo: {},
+        userOrderList: [],
+    };
+};
 
 const user = {
-    state : getDefaultState(),
+    state: getDefaultState(),
 
     mutations: {
-        reset_state: function(state) {
+        reset_state: function (state) {
             state.token = '',
-            state.userId = '',
-            state.userInfo = {
-                
-            }
+                state.userId = '',
+                state.userInfo = {};
         },
-        set_token: function(state, token){
-            state.token = token
+        set_token: function (state, token) {
+            state.token = token;
         },
         set_email: (state, data) => {
-            state.email = data
+            state.email = data;
         },
         set_userId: (state, data) => {
-            state.userId = data
+            state.userId = data;
         },
         set_userInfo: (state, data) => {
             state.userInfo = {
                 ...state.userInfo,
-                ...data
-            }
+                ...data,
+            };
         },
     },
 
     actions: {
-        login: async ({ dispatch, commit }, userData) => {
-            const res = await loginAPI(userData)
-            if(res){
-                setToken(res.id)
-                commit('set_userId', res.id)
-                router.push('/story/content')
-                message.success('登录成功')
-            }else {
-                message.success('登录失败')
-            }
+        login: async ({dispatch, commit}, userData) => {
+            const res = await loginAPI(userData);
+            router.push('/story/content');
         },
-        register: async({ commit }, data) => {
-            const res = await registerAPI(data)
-            if(res){
-                message.success('注册成功')
-            }
+        register: async ({commit}, data) => {
+            const res = await registerAPI(data);
         },
-        getUserInfo({ state, commit }) {
+        getUserInfo({state, commit}) {
             return new Promise((resolve, reject) => {
-              getUserInfoAPI(state.userId).then(response => {
-                const data = response
-                if (!data) {
-                  reject('登录已过期，请重新登录')
-                }
-                commit('set_userInfo', data)
-                commit('set_userId', data.id)
-                resolve(data)
-              }).catch(error => {
-                reject(error)
-              })
-            })
+                getUserInfoAPI(state.userId).then(response => {
+                    const data = response;
+                    if (!data) {
+                        reject('登录已过期，请重新登录');
+                    }
+                    commit('set_userInfo', data);
+                    commit('set_userId', data.id);
+                    resolve(data);
+                }).catch(error => {
+                    reject(error);
+                });
+            });
         },
-        updateUserInfo: async({ state, dispatch }, data) => {
+        updateUserInfo: async ({state, dispatch}, data) => {
             const params = {
                 id: state.userId,
                 ...data,
-            }
-            const res = await updateUserInfoAPI(params)
-            if(res){
-                message.success('修改成功')
-                dispatch('getUserInfo')
+            };
+            const res = await updateUserInfoAPI(params);
+            if (res) {
+                message.success('修改成功');
+                dispatch('getUserInfo');
             }
         },
-        logout: async({ commit }) => {
-            removeToken()
-            resetRouter()
-            commit('reset_state')
+        logout: async ({commit}) => {
+            removeToken();
+            resetRouter();
+            commit('reset_state');
         },
-          // remove token
-        resetToken({ commit }) {
+        // remove token
+        resetToken({commit}) {
             return new Promise(resolve => {
-                removeToken() // must remove  token  first
-                commit('reset_state')
-                resolve()
-            })
+                removeToken(); // must remove  token  first
+                commit('reset_state');
+                resolve();
+            });
         },
-    }
-}
+    },
+};
 
-export default user
+export default user;
