564a349adc8db1da7023266113498f73217ad0c3
Feng Bojuan <181250030@smail.nju.edu.cn>
2020-06-15 23:15:01 +0800

    feat: storyList首页 fix: 部分页面跳转问题（未完成页面部分跳转暂不考虑）

--- src/store/modules/story.js
+++ src/store/modules/story.js
@@ -3,9 +3,11 @@ import {
     recreateAPI,
 } from '@/api/story'
 import { message } from 'ant-design-vue'
+import {getAllStoryAPI} from "../../api/story";
 
 const story = {
     state: {
+        storyList:[],
         recreateList: [
         ],
         Visible: false,
@@ -23,9 +25,16 @@ const story = {
         ]
     },
     mutations: {
+        set_storyList:function (state,data) {
+            state.storyList=data
+        }
 
     },
     actions: {
+        getStoryList:async ({state,commit})=>{
+            const res=await getAllStoryAPI()
+            commit('set_storyList',res)
+        }
 
     }
 }
