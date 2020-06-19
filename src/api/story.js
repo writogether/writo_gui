202b31161bf1b51e3ec5322c6b267c607967bdeb
Turmiht <g13395930368@gmail.com>
2020-06-19 10:07:02 +0800

    fix: api for story sorting changed

--- src/api/story.js
+++ src/api/story.js
@@ -1,74 +1,51 @@
-import { axios } from '../utils/request'
+import {axios} from '../utils/request';
+
 const api = {
-    storyPre: '/api/story'
-}
+    storyPre: '/api/story',
+};
+
 export function getAllStoryAPI() {
     return axios({
         url: `${api.storyPre}/allStory`,
         method: 'GET',
-    })
+    });
 }
-export function getSuspenseAPI() {
+
+export function getByTagAPI(data) {
     return axios({
-        url: `${api.storyPre}/by-type?tag=Suspense`,
+        url: `${api.storyPre}/by-tag?tag=${data.tag}`,
         method: 'GET',
-    })
+    });
 }
-export function getAdventureAPI() {
-    return axios({
-        url: `${api.storyPre}/by-type?tag=Adventure`,
-        method: 'GET',
-    })
-}
-export function getOtherAPI() {
-    return axios({
-        url: `${api.storyPre}/by-type?tag=Other`,
-        method: 'GET',
-    })
-}
-export function getHorrorAPI() {
-    return axios({
-        url: `${api.storyPre}/by-type?tag=Horror`,
-        method: 'GET',
-    })
-}
-export function getRomanticAPI() {
-    return axios({
-        url: `${api.storyPre}/by-type?tag=Romantic`,
-        method: 'GET',
-    })
-}
-export function getFunnyAPI() {
-    return axios({
-        url: `${api.storyPre}/by-type?tag=Funny`,
-        method: 'GET',
-    })
-}
+
 export function uploadNewStoryAPI(data) {
     return axios({
-        url:`${api.storyPre}/upload`,
-        method:'POST',
-        data
-    })
+        url: `${api.storyPre}/upload`,
+        method: 'POST',
+        data,
+    });
 
 }
+
 export function getStoryByIdAPI(id) {
     return axios({
         url: `${api.storyPre}/storyInfo/${id}`,
         method: 'GET',
-    })
+    });
 }
+
 export function getStoryContentAPI(id) {
     return axios({
         url: `${api.storyPre}/content/${id}`,
         method: 'GET',
-    })
+    });
 }
+
 export function recreateAPI(data) {
     return axios({
         url: `${api.storyPre}/recreate`,
         method: 'POST',
         data,
-    })
+    });
 }
 
--- src/views/story/storyList.vue
+++ src/views/story/storyList.vue
@@ -1,187 +1,164 @@
 <template>
     <div style="padding-top: 150px ;width:75%;margin: 0 auto">
-    <div class="storyList">
-        <div style="background: #ffffff;padding: 20px 40px;border-top-left-radius: 10px;border-top-right-radius: 10px">
-            <span style="font-size: large ">选择故事类型：</span>
-            <a-select v-model="storyType"
-                v-decorator="[
-                'storyType',
-                { rules: [{ required: true, message: '请选择故事类型' }] }]"
-                @change="changeStoryType"
-
-            >
-                <a-select-option value='0'>All Stories</a-select-option>
-                <a-select-option value='1'>Adventure</a-select-option>
-                <a-select-option value='2'>Romantic</a-select-option>
-                <a-select-option value='3'>Suspense</a-select-option>
-                <a-select-option value='4'>Other</a-select-option>
-                <a-select-option value='5'>Horror</a-select-option>
-                <a-select-option value='6'>Funny</a-select-option>
-            </a-select>
-
-        <a-button  @click="write" size="large"  shape="round" style="float: right">
-            <a-icon type="plus"/>
-            创作你的新故事！
-        </a-button></div>
-        <div class="table" >
-        <a-list
-            :data-source="storyList"
-            bordered
-        >
-            <a-list-item slot="renderItem" slot-scope="item">
-                <div style="width: 10%;text-align: center">热度{{item.popularity}}</div>
-                <div style="width: 40%;text-align: center">《{{ item.title }}》</div>
-                <div style="width: 10%;text-align: center"  v-if="item.depth>0">第{{item.depth}}续</div>
-                <div style="width: 10%;text-align: center" v-else>首篇</div>
-                <div style="width: 20%;text-align: center">{{item.tag}}</div>
-                <div style="width: 5%"></div>
-                <a-button  shape="round" size="small"  @click="writogether(item)">WriTogether!</a-button>
-            </a-list-item>
-        </a-list>
+        <div class="storyList">
+            <div style="background: #ffffff;padding: 20px 40px;border-top-left-radius: 10px;border-top-right-radius: 10px">
+                <span style="font-size: large ">选择故事类型：</span>
+                <a-select v-model="storyType"
+                          v-decorator="[
+                              'storyType',
+                              { rules: [{ required: true, message: '请选择故事类型' }] }]"
+                          @change="changeStoryType"
+                >
+                    <a-select-option value='All'>All Stories</a-select-option>
+                    <a-select-option value='Adventure'>Adventure</a-select-option>
+                    <a-select-option value='Romantic'>Romantic</a-select-option>
+                    <a-select-option value='Suspense'>Suspense</a-select-option>
+                    <a-select-option value='Other'>Other</a-select-option>
+                    <a-select-option value='Horror'>Horror</a-select-option>
+                    <a-select-option value='Funny'>Funny</a-select-option>
+                </a-select>
+                
+                <a-button @click="write" size="large" shape="round" style="float: right">
+                    <a-icon type="plus" />
+                    创作你的新故事！
+                </a-button>
+            </div>
+            <div class="table">
+                <a-list
+                    :data-source="storyList"
+                    bordered
+                >
+                    <a-list-item slot="renderItem" slot-scope="item">
+                        <div style="width: 10%;text-align: center">热度{{item.popularity}}</div>
+                        <div style="width: 40%;text-align: center">《{{ item.title }}》</div>
+                        <div style="width: 10%;text-align: center" v-if="item.depth>0">第{{item.depth}}续</div>
+                        <div style="width: 10%;text-align: center" v-else>首篇</div>
+                        <div style="width: 20%;text-align: center">{{item.tag}}</div>
+                        <div style="width: 5%"></div>
+                        <a-button shape="round" size="small" @click="writogether(item)">WriTogether!</a-button>
+                    </a-list-item>
+                </a-list>
+            </div>
+            <QuickCreateModal></QuickCreateModal>
         </div>
-        <QuickCreateModal></QuickCreateModal>
-    </div>
     </div>
 </template>
 
 <script>
-    import { mapGetters, mapActions, mapMutations } from 'vuex'
-    import QuickCreateModal from './quickCreate'
-    const columns_storyList=[
+    import {mapActions, mapGetters, mapMutations} from 'vuex';
+    import QuickCreateModal from './quickCreate';
+
+    const columns_storyList = [
         {
-            title:'热度',
-            dataIndex:'popularity',
-            width:'10%',
+            title: '热度',
+            dataIndex: 'popularity',
+            width: '10%',
         },
         {
-            title:'标题',
-            dataIndex:'title',
-            width:'40%'
+            title: '标题',
+            dataIndex: 'title',
+            width: '40%',
         },
         {
-            title:'续写者',
-            dataIndex:'authorId',
+            title: '续写者',
+            dataIndex: 'authorId',
         },
         {
-            title:'类型',
-            dataIndex:'tag'
+            title: '类型',
+            dataIndex: 'tag',
         },
         {
-            title:'续次',
-            dataIndex:'depth'
+            title: '续次',
+            dataIndex: 'depth',
         },
         {
             key: 'action',
-            scopedSlots: { customRender: 'action' },
+            scopedSlots: {customRender: 'action'},
         },
 
     ];
     export default {
-        name: "storyList",
-        components:{
-            QuickCreateModal
+        name: 'storyList',
+        components: {
+            QuickCreateModal,
         },
-        data(){
-            return{
-                like_count:0,
-                dislike_count:0,
-                collect:0,
+        data() {
+            return {
+                like_count: 0,
+                dislike_count: 0,
+                collect: 0,
                 columns_storyList,
-                storyType:'All Stories',
-                popularity:0,
-                pagination:{
-                    pageSize:6
-                }
-            }
+                storyType: 'All Stories',
+                popularity: 0,
+                pagination: {
+                    pageSize: 6,
+                },
+            };
         },
-         mounted(){
-             this.getStoryList()
-            console.log(this.storyList)
+        mounted() {
+            this.getStoryList();
+            console.log(this.storyList);
         },
-        computed:{
+        computed: {
             ...mapGetters(
                 [
                     'storyList',
-                ]
-            )
+                ],
+            ),
         },
-        methods:{
+        methods: {
             ...mapMutations(
                 [
                     'set_storyList',
-                    'set_quickCreateModalVisible'
-                ]
+                    'set_quickCreateModalVisible',
+                ],
             ),
             ...mapActions(
                 [
                     'getStoryList',
-                    'getAdventure',
-                    'getRomantic',
-                    'getSuspense',
-                    'getOther',
-                    'getHorror',
-                    'getFunny'
-                ]
+                    'getByTag',
+                ],
             ),
-            async changeStoryType(v){
-                this.storyType=v
-                switch (v) {
-                    case '0':
-                        await this.getStoryList();
-                        break;
-                    case '1':
-                        await this.getAdventure();break;
-                    case '2':
-                        await this.getRomantic();break;
-                    case '3':
-                        await this.getSuspense();break;
-                    case '4':
-                        await this.getOther();break;
-                    case '5':
-                        await this.getHorror();break;
-                    case '6':
-                        await this.getFunny();break;
-                }
+            async changeStoryType() {
+                if (this.storyType === 'All') this.getStoryList();
+                else this.getByTag({tag:this.storyType});
             },
 
-
-            write(){
+            write() {
                 this.set_quickCreateModalVisible(true);
             },
-            writogether(record){
-                console.log(record)
+            writogether(record) {
+                console.log(record);
                 this.$router.push({
-                    path:'/storyContent',
-                    name:'content',
-                    params:{
-                        id:record.id
-                    }
-                })
-            }
+                    path: '/storyContent',
+                    name: 'content',
+                    params: {
+                        id: record.id,
+                    },
+                });
+            },
 
         },
 
-
-
-    }
+    };
 </script>
 
 <style scoped lang="less">
     .storyList {
-
+        
         border-radius: 10px;
         padding: 6px 6px;
         position: static;
-        background-color:#313c5b;
-
-        .table{
+        background-color: #313c5b;
+        
+        .table {
             background: #f0f2f5;
             padding: 40px 20px;
             border-bottom-left-radius: 10px;
             border-bottom-right-radius: 10px;
-
+            
         }
-
-
+        
+        
     }
 </style>
