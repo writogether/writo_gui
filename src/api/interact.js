c62da6d4334c57e113a58c5836b05fc6bb3919c9
Turmiht <g13395930368@gmail.com>
2020-06-19 11:51:14 +0800

    fix: typo

--- src/api/interact.js
+++ src/api/interact.js
@@ -1,40 +1,40 @@
 import {axios} from '../utils/request';
 
 const api = {
-    interectPre: '/api/interect',
+    interactPre: '/api/interact',
 };
 
 export function commentAPI(data) {
     return axios.post(
-        `${api.interectPre}/comment`,
+        `${api.interactPre}/comment`,
         data,
     );
 }
 
 export function evaluateAPI(data) {
     return axios.post(
-        `${api.interectPre}/evaluate`,
+        `${api.interactPre}/evaluate`,
         data,
     );
 }
 
 export function toggleCollectAPI(data) {
     return axios.post(
-        `${api.interectPre}/collect?storyId=${data.storyId}`,
+        `${api.interactPre}/collect?storyId=${data.storyId}`,
         data,
     );
 }
 
 export function getCommentAPI(data) {
     return axios.get(
-        `${api.interectPre}/comment?storyId=${data.storyId}`,
+        `${api.interactPre}/comment?storyId=${data.storyId}`,
         data,
     );
 }
 
 export function getCollection(data) {
     return axios.get(
-        `${api.interectPre}/collect`,
+        `${api.interactPre}/collect`,
         data,
     );
 }
